f0d49a3a8b6e4a4902582d37941d229df0365901
Fampat <mail@fampat.de>
2020-05-23 21:58:21 +0200

    MOD Another tweak

--- FortifAIConfig.lua
+++ FortifAIConfig.lua
@@ -8,4 +8,4 @@
 -- Scale the entire effect of the AI-benefits
 -- Scale ranges from 0.0 to 2.0, a value of 2.0 result in an unbeatable war-AI
 -- Lesser value provides lesser hardening, higher is more tought to crack AI
-effectScaleValue = 0.25; -- Just change the number, keep the format X.XX
+effectScaleValue = 0.50; -- Just change the number, keep the format X.XX
